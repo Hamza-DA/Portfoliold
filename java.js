5eb041ab61453bead0a9d8dc8b0be0486846cffc
Hamza-DA <60110301+Hamza-DA@users.noreply.github.com>
2020-05-27 01:38:30 +0100

    Add files via upload

--- java.js
+++ java.js
@@ -4,12 +4,10 @@ const navSlide = () => {
   const navlink = document.querySelectorAll('.back li');
   const container = document.querySelector('.container');
 
-
   //toggle nav
   burger.addEventListener('click',()=>{
     nav.classList.toggle('nav-active')
     container.classList.toggle('container-desa')
-
 
   //animation
   navlink.forEach((link, index) => {
@@ -22,3 +20,18 @@ const navSlide = () => {
   })
 }
 navSlide();
+
+const formslid = () => {
+  const formdea = document.querySelector('.form-active');
+  const boutton = document.getElementById('cbutn');
+  const form = document.querySelector('.form');
+  const fcross = document.querySelector('.navc');
+
+  boutton.addEventListener('click',()=>{
+    form.classList.toggle('form-active')
+  })
+  fcross.addEventListener('click', ()=>{
+    formdea.classList.toggle('form-active')
+  })
+}
+formslid();
